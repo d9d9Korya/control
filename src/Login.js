c3a39a11e9cba955e6c3abebea4476befe9d5314
korneyqq <novikov-korney@inbox.ru>
2019-11-20 17:47:58 +0300

    loginchanges

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import App from './App';
+import Form from './View';
 
 export default class Login extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ export default class Login extends Component {
   }
 
   handleSubmit(event) {
-    
+
   }
 
   render() {
@@ -47,7 +47,7 @@ export default class Login extends Component {
             required
           />
 
-          <button type="submit">Login</button>
+          <button type="submit" >Login</button>
         </form>
       </div>
     );
